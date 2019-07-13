3621bd8b85522fbe04eac7970812b9279c303cb1
gnought <1684105+gnought@users.noreply.github.com>
2019-07-13 17:30:09 +0800

    Not to mix && and ||

--- packet.js
+++ packet.js
@@ -3,7 +3,7 @@
 function Packet (original, broker) {
   this.cmd = original.cmd || 'publish'
   this.brokerId = original.brokerId || (broker && broker.id)
-  this.brokerCounter = original.brokerCounter || (broker && (++broker.counter) || 0)
+  this.brokerCounter = original.brokerCounter || (broker ? (++broker.counter) : 0)
   this.topic = original.topic
   this.payload = original.payload || new Buffer(0)
   this.qos = original.qos || 0
