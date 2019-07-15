7286489092771baab338a243674b68b9a1f98a9a
Gnought <1684105+gnought@users.noreply.github.com>
2019-07-15 13:18:22 +0800

    Fixed typo

--- packet.js
+++ packet.js
@@ -8,7 +8,7 @@ function Packet (original, broker) {
   this.payload = original.payload || Buffer.alloc(0)
   this.qos = original.qos || 0
   this.retain = original.retain || false
-  this.dup = original.dup || false
+  this.dup = origianl.dup || false
   // [MQTT-2.3.1-5]
   if (this.qos > 0 || this.cmd !== 'publish') {
     //  [MQTT-2.3.1-1]
